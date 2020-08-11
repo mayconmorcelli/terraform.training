384f5b3ad56202f972ff25eaa6b2de0146a39a22
mayconmorcelli <69490350+mayconmorcelli@users.noreply.github.com>
2020-08-10 23:40:06 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,3 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
-
-[23:33] Diógenes Luís Ramos de Souza
-    git config --global user.name "maycon.morcelli"
-
-git config --global user.password "senha91688!"
