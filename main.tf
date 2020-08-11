b6d277903bac54f8d8c5a976c7b9a0e7aaaa9cf3
maycon.morcelli <maycon.morcelli@pampa.compasso>
2020-08-10 23:38:58 -0300

    teste commit

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+[23:33] Diógenes Luís Ramos de Souza
+    git config --global user.name "maycon.morcelli"
+
+git config --global user.password "senha91688!"
